56f62824ae017b424b13adc01faf33e049633847
Marcelo Forclaz <marcelo.forclaz@stensul.com>
2024-11-17 20:15:41 -0300

    fix

--- update.bat
+++ update.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 echo Removing package-lock.json and node_modules...
 if exist frontend\package-lock.json del frontend\package-lock.json
-if exist frontend\node_modules rmdir /s /q frontend/node_modules
+if exist frontend\node_modules rmdir /s /q frontend\node_modules
 
 echo Restoring all current changes in git...
 git restore . 

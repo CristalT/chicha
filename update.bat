8108974651c3f3072dc4599b73a0477d9f7a40d4
Marcelo Forclaz <marcelo.forclaz@stensul.com>
2024-11-17 20:12:48 -0300

    batch script for updating system

---
+++ update.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Removing package-lock.json and node_modules...
+if exist frontend/package-lock.json del frontend/package-lock.json
+if exist frontend/node_modules rmdir /s /q frontend/node_modules
+
+echo Restoring all current changes in git...
+git restore . 
+git clean -fd
+
+echo Pulling latest changes from origin/main...
+git pull origin main
+
+echo Compiling ...
+wails build
+
+echo Process completed.
+pause
